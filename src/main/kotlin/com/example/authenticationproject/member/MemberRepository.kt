952a4c82ae4c63b00ca04f592ad22f1958d2dc5a
RudySeo <gun100491@gmail.com>
2025-05-02 19:39:06 +0900

    Feat: 리퍼지토리 추가

---
+++ src/main/kotlin/com/example/authenticationproject/member/MemberRepository.kt
@@ -0,0 +1,9 @@
+package com.example.authenticationproject.member
+
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface MemberRepository : JpaRepository<Member, Long> {
+
+}

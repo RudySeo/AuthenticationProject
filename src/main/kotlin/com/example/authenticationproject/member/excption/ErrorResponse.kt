cb22979a07ad4cbaf966ff2eb23e90c1581b6f4d
RudySeo <gun100491@gmail.com>
2025-05-03 23:00:16 +0900

    feat(exception): 글로벌 예외처리 추가

---
+++ src/main/kotlin/com/example/authenticationproject/member/excption/ErrorResponse.kt
@@ -0,0 +1,6 @@
+package com.example.authenticationproject.member.excption
+
+data class ErrorResponse(
+    val code: String,
+    val message: String
+)
---
+++ src/main/kotlin/com/example/authenticationproject/member/excption/GlobalExceptionHandler.kt
@@ -0,0 +1,38 @@
+import com.example.authenticationproject.member.excption.ErrorResponse
+import org.hibernate.exception.ConstraintViolationException
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+
+@RestControllerAdvice
+class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException::class)
+    fun handleIllegalArgument(ex: IllegalArgumentException): ResponseEntity<ErrorResponse> {
+        val error = ErrorResponse(
+            code = "400",
+            message = ex.message ?: "잘못된 요청입니다."
+        )
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error)
+    }
+
+    @ExceptionHandler(ConstraintViolationException::class)
+    fun handleConstraintViolation(ex: ConstraintViolationException): ResponseEntity<ErrorResponse> {
+        val error = ErrorResponse(
+            code = "400",
+            message = "검증에 실패했습니다: ${ex.message}"
+        )
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error)
+    }
+
+    @ExceptionHandler(Exception::class)
+    fun handleGenericException(ex: Exception): ResponseEntity<ErrorResponse> {
+        val error = ErrorResponse(
+            code = "500",
+            message = "서버 내부 오류가 발생했습니다. (${ex.localizedMessage})"
+        )
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error)
+    }
+}

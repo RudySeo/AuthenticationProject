5ac11ea9c66eb70a2c161d8316cf7f8daf5ffbd9
RudySeo <gun100491@gmail.com>
2025-06-14 22:43:30 +0900

    feat(member): login token 추가

--- src/main/kotlin/com/example/authenticationproject/member/MemberService.kt
+++ src/main/kotlin/com/example/authenticationproject/member/MemberService.kt
@@ -1,17 +1,25 @@
 package com.example.authenticationproject.member
 
+import com.example.authenticationproject.utill.jwt.JwtTokenProvider
 import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.Authentication
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 
 @Service
 class MemberService(
     private val memberRepository: MemberRepository,
+    private val authenticationManager: AuthenticationManager,
+    private val jwtTokenProvider: JwtTokenProvider,
+    private val passwordEncoder: PasswordEncoder
 ) {
 
     fun signup(request: MemberDto.SignupRequest) {
         val user = Member(
             username = request.username,
-            password = request.password,
+            password = passwordEncoder.encode(request.password),
             email = request.email,
             role = Role.USER,
         )
@@ -19,12 +27,20 @@ class MemberService(
     }
 
     fun login(request: MemberDto.LoginRequest): MemberDto.LoginResponse {
+        // 1. 인증 토큰 생성
+        val authToken = UsernamePasswordAuthenticationToken(request.email, request.password)
 
-        val findMember =
-            memberRepository.findByEmail(request.email).orElseThrow { NotFoundException() }
+        // 2. 인증 수행
+        val authentication: Authentication = authenticationManager.authenticate(authToken)
 
-        return MemberDto.LoginResponse(email = findMember.email)
+        // 3. 토큰 발급
+        val accessToken = jwtTokenProvider.generateAccessToken(authentication)
+        val refreshToken = jwtTokenProvider.generateRefreshToken(authentication)
 
+        return MemberDto.LoginResponse(
+            accessToken = accessToken,
+            refreshToken = refreshToken
+        )
     }
 
     fun getMemberById(id: Long): MemberDto.MemberResponse {
